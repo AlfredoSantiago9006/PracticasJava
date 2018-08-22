942ae5842b163984dd674514a4422a34fca0ec1b
AlfredoSantiago9006 <josantiago.datic@utez.edu.mx>
2018-08-21 20:58:09 -0500

    Practicas de java

---
+++ ejercicio_4/Main.java
@@ -0,0 +1,43 @@
+package ejercicio_4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Persona person0 = new Usuario("Alfredo","alfredo1@mail.com");
+		Persona person1 = new Usuario("Carolina","caro@mail.com");
+		Persona person2 = new Usuario("Roberto","roberto@mail.com");
+		Persona person3 = new Usuario("Natalia","natalia@mail.com");
+		Persona person4 = new Administrador("Miriam","miriam@mail.com");
+		Persona person5 = new Administrador("Cristina","cris@mail.com");
+		Persona person6 = new Administrador("Andrea","andrea@mail.com");
+		Persona person7 = new Administrador("Miguel","migue@mail.com");
+		
+		List<Persona> l = new ArrayList<Persona>();
+		
+		l.add(person0);
+		l.add(person1);
+		l.add(person2);
+		l.add(person3);
+		l.add(person4);
+		l.add(person5);
+		l.add(person6);
+		l.add(person7);
+		
+		EnvioCorreos ec = new EnvioCorreos();
+		ec.enviarCorreo(l);
+		
+		Map<Integer, Persona> map = new HashMap<Integer, Persona>();
+		map.put(1, person0);
+		map.put(2, person1);
+		
+		ec.enviarCorreoId(map);
+		
+		
+	}
+
+}
