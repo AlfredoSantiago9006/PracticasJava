9f512a3fb1ff2e8f5e9414e1022e0953cd5dfeaf
AlfredoSantiago9006 <josantiago.datic@utez.edu.mx>
2018-08-24 14:46:39 -0500

    Practica HolaMundo con javadoc

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @author alfre
+ *
+ *Clase HolaMundo imprime en consola HolaMundo
+ */
+public class HolaMundo {
+	public static void main (String args[]) {
+		System.out.println("Hola Mundo");
+	}
+}
